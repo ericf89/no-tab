0fb13c63a10ba485b063bb09d350d4b5a6ce52a2
Eric Fennell <efennell@thezebra.com>
2016-02-01 10:59:57 -0600

    Adding example page

---
+++ index.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { setupViewContainer, formInput } from 'no-tab';
+const { setupFormInput } = formInput;
+
+const Input = setupFormInput(props => <input {...props}/>);
+
+const BasicExampleView = setupViewContainer(
+    () => (
+        <div className="example-view">
+            <h3>Basic Example</h3>
+            <Input placeholder="formId a" formId="a"/>
+            <Input placeholder="formId b" formId="b"/>
+            <Input placeholder="formId c" formId="c"/>
+            <Input placeholder="formId d" formId="d"/>
+        </div>
+    )
+);
+
+const MultiInputFormExampleView = setupViewContainer(
+    () => (
+        <div className="example-view">
+            <h3>Matching FormId Example</h3>
+            <Input placeholder="formId a" formId="a"/>
+            <Input placeholder="formId a" formId="a"/>
+            <Input placeholder="formId b" formId="b"/>
+            <Input placeholder="formId b" formId="b"/>
+        </div>
+    )
+);
+
+ReactDOM.render(
+    <div>
+    <h1>NoTab Examples</h1>
+    <BasicExampleView/>
+    <MultiInputFormExampleView/>
+    </div>,
+    document.getElementById('example')
+);
+
